d6cf5e7a35bab0cff677ab70bb88b9d4bbf22231
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 21:22:12 +0100

    add tests for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:foods) }
+  it { should have_many(:treats) }
+  it { should have_many(:walks) }
+  it { should have_many(:medications) }
+  it { should have_many(:general_healths) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:breed) }
+  it { should validate_presence_of(:weight) }
+  it { should validate_presence_of(:date_of_birth) }
 end
