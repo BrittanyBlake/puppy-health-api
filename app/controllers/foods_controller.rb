572b1b882b574b8f1cf5bee11ddc2f53a46cb80b
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 13:09:05 +0100

    add food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,2 +1,43 @@
 class FoodsController < ApplicationController
+    before_action :set_food, only: [:show, :update, :destroy]
+
+    #GET /foods
+    def index
+        @foods = Food.all
+        json_response(@foods)
+    end
+
+     # POST /foods
+  def create
+    @food = Food.create!(food_params)
+    json_response(@food, :created)
+  end
+
+  # GET /foods/:id
+  def show
+    json_response(@food)
+  end
+
+  # PUT /foods/:id
+  def update
+    @food.update(food_params)
+    head :no_content
+  end
+
+  # DELETE /foods/:id
+  def destroy
+    @food.destroy
+    head :no_content
+  end
+
+  private
+
+  def food_params
+    # whitelist params
+    params.permit(:brand, :amount, :date, :time)
+  end
+
+  def set_food
+    @food = Food.find(params[:id])
+  end
 end
