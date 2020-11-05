f34028d3c7e7a57621ef3e852a5e07b056f9a475
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 16:06:58 +0100

    add specs for treats odel

--- spec/models/treat_spec.rb
+++ spec/models/treat_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Treat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:type)}
+  it { should validate_presence_of(:amount)}
+  it { should validate_presence_of(:date)}
 end
