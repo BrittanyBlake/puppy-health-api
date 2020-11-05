b29ac74c3c58b32a08ab3d28c9f07f0419a394db
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 16:49:09 +0100

    generate walk model and tests

---
+++ app/models/walk.rb
@@ -0,0 +1,2 @@
+class Walk < ApplicationRecord
+end
---
+++ spec/models/walk_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Walk, type: :model do
+  it { should validate_presence_of(:time)}
+  it { should validate_presence_of(:distance)}
+  it { should validate_presence_of(:date)}
+end
