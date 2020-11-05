77cbade5f146e9f6b00609eeb2f22da8e72e9b8e
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 20:50:07 +0100

    add validations to general health model

--- app/models/general_health.rb
+++ app/models/general_health.rb
@@ -1,2 +1,3 @@
 class GeneralHealth < ApplicationRecord
+    validates_presence_of :date, :energy_level, :hunger_level
 end
--- spec/models/general_health_spec.rb
+++ spec/models/general_health_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GeneralHealth, type: :model do
-  it { should validate_presence_of(:weight)}
   it { should validate_presence_of(:energy_level)}
   it { should validate_presence_of(:hunger_level)}
+  it { should validate_presence_of(:date)}
 end
