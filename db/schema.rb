b2981b78b2a00a952ac51a88e2960c1735bae518
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 21:20:03 +0100

    add birthday to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_05_201413) do
+ActiveRecord::Schema.define(version: 2020_11_05_201749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2020_11_05_201413) do
     t.integer "current_weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "date_of_birth"
   end
 
   create_table "walks", force: :cascade do |t|
