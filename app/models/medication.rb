1dcd1371b0b89844442e1d9825b8853282b39089
Brittany Blake <brittany.blake49@gmail.com>
2020-11-05 20:06:18 +0100

    add validations and spec for medications

--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,2 +1,3 @@
 class Medication < ApplicationRecord
+    validates_presence_of :name, :use, :dosage
 end
--- spec/models/medication_spec.rb
+++ spec/models/medication_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Medication, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:use)}
+  it { should validate_presence_of(:dosage)}
 end
