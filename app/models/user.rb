41fa12a56e5ab95da46641a72c286bb86e10766e
Brittany Blake <brittany.blake49@gmail.com>
2020-11-06 08:50:50 +0100

    add associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :foods
+    has_many :treats
+    validates_presence_of :name, :email, :password_digest, :breed, :weight, :date_of_birth
+
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    name {Faker::Creature::Dog.name}
+    email 'foo@bar.com'
+    password 'foobar'
+    breed {Faker::Creature::Dog.breed }
+    weight {Faker::Number.between(from: 1, to: 10)}
+    date_of_birth {Faker::Date.birthday(min_age: 1, max_age: 6)}
+  end
+end
