289fe58c462eb32e10bd71bafd29c0868f355c52
Brittany Blake <brittany.blake49@gmail.com>
2020-11-14 18:26:26 +0100

    fix linters

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,4 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:password_digest) }
-  it { should validate_presence_of(:breed) }
-  it { should validate_presence_of(:goal_weight) }
-  it { should validate_presence_of(:current_weight) }
-  it { should validate_presence_of(:date_of_birth) }
 end
